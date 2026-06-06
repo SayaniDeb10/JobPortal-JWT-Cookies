64674c22dc95167fe4d6fb37b63d0ce6bce5983b
Sayani Deb <Sayanideb10@gmail.com>
2026-06-06 14:14:02 +0530

    Creating login page for user

---
+++ CrudOperation/Dto/StudentDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudOperation.Dto
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+
+        public string StudentName { get; set; } = null!;
+
+        public string Gender { get; set; } = null!;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age { get; set; }
+
+        public string Qualifiaction { get; set; } = null!;
+
+        public string Language { get; set; } = null!;
+
+        public string Location { get; set; } = null!;
+    }
+}
